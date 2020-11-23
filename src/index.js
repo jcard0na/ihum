f2275d8701f5274be599a1a21524e1ba4b5003f9
Javier Cardona <javier@okio.io>
2020-11-23 22:24:20 +0000

    Record an audio blob

--- src/index.js
+++ src/index.js
@@ -34,18 +34,51 @@ class HumApp extends React.Component {
         }
         this.synth = new Tone.Synth().toDestination();
 
+        this.chunks = []
+
         this.startStop = this.startStop.bind(this);
+        this.captureInputStream = this.captureInputStream.bind(this);
+    }
+
+    captureInputStream() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            console.log('getUserMedia supported.');
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then((stream) => {
+                    this.mediaRecorder = new MediaRecorder(stream);
+
+                    this.mediaRecorder.ondataavailable = (e) => {
+                            console.log(`recorded ${e.data.size} chunks`)
+                            this.chunks.push(e.data);
+                    }
+                    this.mediaRecorder.onstop = (e) => {
+                            console.log(`stopped recording`)
+                    }
+                })
+                .catch(function (err) {
+                    console.log('The following getUserMedia error occured: ' + err);
+                }
+                );
+        } else {
+            console.log('getUserMedia not supported on your browser!');
+        }
+    }
+     
+    componentDidMount() { 
+        this.captureInputStream() 
     }
 
     startStop() {
         const INTERVAL = 2000
-        console.log(this.state.isRunning)
         if (this.state.isRunning) {
             clearInterval(this.timer);
         } else {
             Tone.start()
             this.synth.triggerAttackRelease(`${this.chords[0]}4`, "8n");
+            this.mediaRecorder.start()
             this.timer = setInterval(() => {
+                if (this.mediaRecorder.state === 'recording')
+                    this.mediaRecorder.stop()
                 let next_idx = (this.state.index + 1) % this.chords.length
                 this.synth.triggerAttackRelease(`${this.chords[next_idx]}4`, "8n");
                 this.setState({ index: next_idx, current: this.chords[next_idx] })
