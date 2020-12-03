90c06d7fdcd1cb420634aeaa57dd5cdbd7f29052
Javier Cardona <javier@okio.io>
2020-12-03 12:32:28 +0000

    Started to work on server-side unit testing

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,47 @@
 const functions = require('firebase-functions')
 const express = require('express')
+var cookieSession = require('cookie-session')
+
+const challenges = [
+      {
+          chord: 'Bb',
+          ask: ['P1', 'M3', 'P5'],
+      },
+      {
+          chord: 'F',
+          ask: ['P1', 'M3', 'P5'],
+      },
+      {
+          chord: 'B',
+          ask: ['P1', 'M3', 'P5'],
+      },
+      {
+          chord: 'D',
+          ask: ['P1', 'M3', 'P5'],
+      },
+      {
+          chord: 'G',
+          ask: ['P1', 'M3', 'P5'],
+      },
+  ];
+
+  var current_challenge = 0;
 
 const app = express()
 
-app.get('*', (req, res) => {
-      res.send("Hello from the API")
+app.use(cookieSession({
+      name: 'session',
+      keys: ['12siehdieosfehf'],
+      // Cookie Options
+      maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+app.get('/next-challenge', (req, res) => {
+      current_challenge = (current_challenge + 1) % challenges.length;
+      res.send(challenges[current_challenge])
 })
 
-exports.api = functions.https.onRequest(app)
+
+
+exports.api = functions.https.onRequest(app);
+exports.app = express;
