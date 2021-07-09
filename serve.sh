36705b8264b8e84d18f166390014849c24b871ad
Javier Cardona <javier@okio.io>
2021-07-09 00:02:43 -0700

    Modify serve.sh to support local backend
    `./serve.sh local` will launch a local backend instead of connecting to
    the one hosted on firebase.
    
    Also fix typo s/ENBALED/ENABLED

--- serve.sh
+++ serve.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
-cd client
-npm run build 
-cd ..
-firebase serve
+if [ "$1" = "local" ]
+then
+    # Run backend locally
+    cd functions
+    npm run start-dev &
+    SERVER_PID=$!
+    cd ..
+    cd client
+    REACT_APP_BACKEND=local npm run start
+    wait $SERVER_PID
+else
+    # Connect to backend hosted in firebase
+    cd client
+    npm run build
+    cd ..
+    firebase serve
+fi
