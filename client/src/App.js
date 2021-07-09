21564b0ee15fd30ee50bad7beff45fbbe21c3d4c
Jeffrey Han <itdelatrisu@gmail.com>
2021-07-08 22:10:40 -0700

    UI code/visual changes
    Code changes:
    - Use `makeStyles`/`useStyles` from material-ui for CSS classes
    - Add `API_URL` constant to make it easy (but still manual) to change back-end endpoint
    - Minor refactoring
    
    Visual changes:
    - Fix html template title/description
    - Replace `Fab` with regular `Button`
    - Use `Card` to wrap all content
    - Add SVG background pattern

--- client/src/App.js
+++ client/src/App.js
@@ -3,54 +3,68 @@ import * as Tone from "tone";
 import fetch from "node-fetch";
 import { stringify } from "query-string";
 
-import { Fab, Button, Card, CardContent, Typography } from "@material-ui/core";
+import { Button, Card, CardContent, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import "./index.css";
 import Chord from "./Chord.js";
 import Checker from "./Checker.js";
 import Timer from "./Timer.js";
 
+// Set API URL (for /challenge endpoint)
+const API_URL = "";
+//const API_URL = "http://localhost:8080";
+
+const CARD_WIDTH_PX = 480;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  card: {
+    width: CARD_WIDTH_PX,
+    marginBottom: theme.spacing(1),
+  },
+  buttonContainer: {
+    width: CARD_WIDTH_PX,
+    "& > *": {
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
+
 function AppButton(props) {
-  if (props.enabled) {
-    if (props.main) {
-      return (
-        <Fab color="primary" variant="contained" onClick={props.onClick}>
-          {props.value}
-        </Fab>
-      );
-    } else {
-      return (
-        <Button color="primary" variant="contained" onClick={props.onClick}>
-          {props.value}
-        </Button>
-      );
-    }
-  } else {
-    return null;
-  }
+  return (
+    props.enabled && (
+      <Button
+        color={props.main ? "primary" : "secondary"}
+        variant="contained"
+        onClick={props.onClick}
+      >
+        {props.value}
+      </Button>
+    )
+  );
 }
 
 function Instructions(props) {
-  if (props.enabled) {
-    return (
-      <div>
-        <Card className="bloh">
-          <CardContent>
-            <Typography className="blah" color="textSecondary" gutterBottom>
-              iHum
-            </Typography>
-            <Typography variant="body2" component="p">
-              When you press 'Start' you will hear a chord. Try to play or sing
-              the individual notes as requested. A new chord will be played on
-              success or after a timeout.
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return (
+    props.enabled && (
+      <>
+        <Typography color="textSecondary" gutterBottom>
+          iHum
+        </Typography>
+        <Typography variant="body2" component="p">
+          When you press 'Start', you will hear a chord. Try to play or sing the
+          individual notes as requested. A new chord will be played on success
+          or after a timeout.
+        </Typography>
+      </>
+    )
+  );
 }
 
 const states = {
@@ -83,6 +97,7 @@ function useInterval(callback, delay) {
 }
 
 function App(props) {
+  const classes = useStyles();
   const [current, setCurrent] = React.useState(states.PAUSED);
   const [challenge, setChallenge] = React.useState({
     chord: { name: null, intervals: null },
@@ -92,10 +107,10 @@ function App(props) {
 
   const nextChallenge = async (result) => {
     const query = { difficulty: 0 };
-    const response = await fetch(`/challenge?${stringify(query)}`);
+    const response = await fetch(`${API_URL}/challenge?${stringify(query)}`);
     const challenge = await response.json();
 
-    console.log(result);
+    console.log(`Result: "${result}"`);
     console.log(challenge);
 
     setChallenge(challenge);
@@ -136,31 +151,33 @@ function App(props) {
   };
 
   return (
-    <div>
-      <div>
-        <Instructions enabled={current === states.PAUSED} />
-      </div>
-      <div>
-        <Chord
-          enabled={current === states.PLAYING}
-          chord={challenge.chord}
-          onDone={checkChallenge}
-        />
-      </div>
-      <div>
-        <Timer
-          enabled={current === states.CHECKING}
-          remaining={Math.round((timeRemaining * 100) / challenge.time)}
-        />
-      </div>
-      <div>
-        <Checker
-          enabled={current === states.CHECKING}
-          challenge={challenge.chord}
-          onDone={nextChallenge}
-        />
-      </div>
-      <div>
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Instructions enabled={current === states.PAUSED} />
+          <div>
+            <Chord
+              enabled={current === states.PLAYING}
+              chord={challenge.chord}
+              onDone={checkChallenge}
+            />
+          </div>
+          <div>
+            <Timer
+              enabled={current === states.CHECKING}
+              remaining={Math.round((timeRemaining * 100) / challenge.time)}
+            />
+          </div>
+          <div>
+            <Checker
+              enabled={current === states.CHECKING}
+              challenge={challenge.chord}
+              onDone={nextChallenge}
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className={classes.buttonContainer}>
         <AppButton
           main={true}
           onClick={startStop}
