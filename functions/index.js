50547599c550dd4a9127620f2be86505ffe1b82c
Jeffrey Han <itdelatrisu@gmail.com>
2021-07-08 22:05:12 -0700

    Add option to run back-end locally
    Add command `npm run start-dev` to launch the Express server locally and
    skip Firebase calls. By default, this uses port 8080.

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,15 @@
-const functions = require("firebase-functions");
 const express = require("express");
 const generator = require("./generator");
 
 var cookieSession = require("cookie-session");
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require("firebase-admin");
-admin.initializeApp();
+const DEV_MODE_ENBALED = process.env.NODE_ENV === "dev";
+
+if (!DEV_MODE_ENBALED) {
+  // The Firebase Admin SDK to access the Firebase Realtime Database.
+  const admin = require("firebase-admin");
+  admin.initializeApp();
+}
 
 const challenges = [
   {
@@ -35,6 +38,15 @@ var current_challenge = 0;
 
 const app = express();
 
+if (DEV_MODE_ENBALED) {
+  // Set CORS headers
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
+}
+
 app.get("/chord", (req, res) => {
   res.send(generator.generateChord(req.query.difficulty));
 });
@@ -43,4 +55,13 @@ app.get("/challenge", (req, res) => {
   res.send(generator.generateChallenge(req.query.difficulty));
 });
 
-exports.api = functions.https.onRequest(app);
+if (DEV_MODE_ENBALED) {
+  // Serve directly
+  const port = process.env.HTTP_PORT || 8080;
+  app.listen(port, () => {
+    console.log(`[DEV MODE] Listening on http://localhost:${port}`);
+  });
+} else {
+  const functions = require("firebase-functions");
+  exports.api = functions.https.onRequest(app);
+}
